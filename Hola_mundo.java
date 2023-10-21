92721e4ad1c972bde60fcb518e1e660561f047a0
Juan24lp <leonpazantonio@gmail.com>
2023-10-21 10:16:23 -0600

    ahs

---
+++ Hola_mundo.java
@@ -0,0 +1,25 @@
+/**
+ * Hola_mundo
+ */
+public class Hola_mundo {
+
+    public static void main(String[] args){
+        System.out.println("Hola mundo shhf"); 
+    
+
+    int a;
+    Integer b;
+    
+    a= 15;
+    b= 23;
+
+    System.out.println(a+b);
+
+    int presio = Integer.parseInt("78");
+    System.out.println(presio);
+
+    String nombre = "Juan Antonio";
+    System.out.println("Hola "+ nombre);
+    }
+
+}
