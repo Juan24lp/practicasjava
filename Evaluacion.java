fdf4250ea47de72c4e600d219c7b189e68aad492
Juan24lp <leonpazantonio@gmail.com>
2024-01-19 16:41:31 -0600

    eje1

--- Evaluacion.java
+++ Evaluacion.java
@@ -1,4 +1,7 @@
+//Juan Antonio Leon Paz.
 public class Evaluacion {
-    
-    
+    public static void main(String[] args){
+        //El metodo main ejecuta las tareas de la clase.
+        System.out.println("¡Mi primer programa Java desde cero!");    
+    }
 }
